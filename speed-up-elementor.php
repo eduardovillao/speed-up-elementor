cc4d5ee0c49356a0288732291228d51bc3d87808
Eduardo Villão <60461940+eduardovillao@users.noreply.github.com>
2020-08-19 12:42:38 -0300

    Update speed-up-elementor.php
    Update code style

--- speed-up-elementor.php
+++ speed-up-elementor.php
@@ -10,9 +10,9 @@ class Speed_Elementor_Optimize {
 		add_action( 'wp_enqueue_scripts', [$this, 'dequeue_if_home_page'], 999999 );
 		add_action( 'init', [$this, 'disable_wp_emojis'] );
 		add_action( 'elementor/frontend/after_register_styles', [$this, 'remove_font_awesome_elementor'], 20 );
-    add_action( 'elementor/frontend/after_register_scripts', [$this, 'remove_elementor_js'], 20 );
-    add_action( 'wp_enqueue_scripts', [ $this, 'remove_elementor_pro_js' ], 20 );
-    add_action( 'elementor/frontend/after_enqueue_styles', [$this, 'remove_style_elementor'], 20 );
+    		add_action( 'elementor/frontend/after_register_scripts', [$this, 'remove_elementor_js'], 20 );
+    		add_action( 'wp_enqueue_scripts', [ $this, 'remove_elementor_pro_js' ], 20 );
+    		add_action( 'elementor/frontend/after_enqueue_styles', [$this, 'remove_style_elementor'], 20 );
 		add_action( 'wp_default_scripts', [$this, 'remove_wp_jquery_migrate'] );
 		add_filter( 'style_loader_src', [$this, 'remove_cssjs_ver'], 999999, 2 );
 		add_filter( 'script_loader_src', [$this, 'remove_cssjs_ver'], 999999, 2 );
@@ -20,74 +20,74 @@ class Speed_Elementor_Optimize {
 
 
 	/**
-	 * 
-	 * Remove CSS and JS if is front page (home)
-   *
-	 * you can change page or what scripts remove, its just a example
-   *
-	 */
+	* 
+	* Remove CSS and JS if is front page (home)
+   	*
+	* you can change page or what scripts remove, its just a example
+   	*
+	*/
 
 	public function dequeue_if_home_page () {
 
 	    if ( is_front_page() ) {
       
-        ////Remove Elementor icons if you dont use
-			  wp_deregister_style( 'elementor-icons' );
+        	////Remove Elementor icons if you dont use
+		wp_deregister_style( 'elementor-icons' );
           
-        ////Remove Dashicons if not logged user
-	      wp_dequeue_style( 'dashicons' );
+        	////Remove Dashicons if not logged user
+		wp_dequeue_style( 'dashicons' );
                     
-        ////Remove WordPress Block Library if you dont use
-        wp_dequeue_style( 'wp-block-library' );
+        	////Remove WordPress Block Library if you dont use
+        	wp_dequeue_style( 'wp-block-library' );
           
-        ////Remove WordPress Block Library theme if you dont use
-			  wp_dequeue_style( 'wp-block-library-theme' );
+        	////Remove WordPress Block Library theme if you dont use
+		wp_dequeue_style( 'wp-block-library-theme' );
 	    }
 	}
   
   
-  /**
-	 * 
-	 * Remove Elementor JS
-   *
-	 * improtant: this remove defaults Elementor JS and some its
-   * required for each function. Disabe each and test for dont 
-   * broke your website functions
-   *
-	 */
+  	/**
+	* 
+	* Remove Elementor JS
+   	*
+	* improtant: this remove defaults Elementor JS and some its
+   	* required for each function. Disabe each and test for dont 
+   	* broke your website functions
+   	*
+	*/
 
 	public function remove_elementor_js () {
 
 		if ( is_front_page() ) {
 
-		  wp_deregister_script( 'elementor-frontend' );
+			wp_deregister_script( 'elementor-frontend' );
 
-		  wp_register_script(
-        'elementor-frontend',
-        ELEMENTOR_ASSETS_URL . 'js/frontend.min.js',
-        [
-          'elementor-frontend-modules',
-          //'elementor-dialog', //Just comment if you want remove
-          'elementor-waypoints',
-          //'swiper', //Example: disable if you dont use slider os others similars in your page
-          //'share-link',
-        ],
-        ELEMENTOR_VERSION,
-        true
+			wp_register_script(
+        		'elementor-frontend',
+        		ELEMENTOR_ASSETS_URL . 'js/frontend.min.js',
+        		[
+          		'elementor-frontend-modules',
+          		//'elementor-dialog', //Just comment if you want remove
+          		'elementor-waypoints',
+          		//'swiper', //Example: disable if you dont use slider os others similars in your page
+          		//'share-link',
+        		],
+        		ELEMENTOR_VERSION,
+        		true
 			);
 		}
 	}
   
   
-    /**
-	 * 
-	 * Remove Elementor PRO JS
-   *
-	 * improtant: this remove defaults Elementor PRO JS and some its
-   * required for each function. Disabe each and test for dont 
-   * broke your website functions
-   *
-	 */
+    	/**
+	* 
+	* Remove Elementor PRO JS
+   	*
+	* improtant: this remove defaults Elementor PRO JS and some its
+   	* required for each function. Disabe each and test for dont 
+   	* broke your website functions
+   	*
+	*/
 
 	public function remove_elementor_pro_js () {
 
@@ -96,27 +96,27 @@ class Speed_Elementor_Optimize {
 			wp_deregister_script( 'elementor-pro-frontend' );
 
 			wp_enqueue_script(
-		    'elementor-pro-frontend',
-		    ELEMENTOR_PRO_URL . 'assets/js/frontend.min.js',
-		    [
-		    'elementor-frontend-modules',
-		    //'elementor-sticky', ////Remove if you dont use element sticky in your page
-		    ],
-		    ELEMENTOR_PRO_VERSION,
-		    true
+			'elementor-pro-frontend',
+			ELEMENTOR_PRO_URL . 'assets/js/frontend.min.js',
+		 	[
+			'elementor-frontend-modules',
+			//'elementor-sticky', ////Remove if you dont use element sticky in your page
+		    	],
+		    	ELEMENTOR_PRO_VERSION,
+		    	true
 			);
 		}
 	}
   
   
-  /**
-	 * 
-	 * Remove Font Awesome icons by Elementor
-   *
-	 * Important: this remove all font awesome icons from Elementor
-   * use SVG icons for best performance
-   *
-	 */
+  	/**
+	* 
+	* Remove Font Awesome icons by Elementor
+   	*
+	* Important: this remove all font awesome icons from Elementor
+   	* use SVG icons for best performance
+   	*
+	*/
 
 	public function remove_font_awesome_elementor () {
 
@@ -126,14 +126,14 @@ class Speed_Elementor_Optimize {
 	}
   
   
-  /**
-	 * 
-	 * Remove others Elementor styles
-   *
-	 * Important: this remove defaults styles from Elementor
-   * Disabe each and test for dont broke your website functions
-   *
-	 */
+  	/**
+	* 
+	* Remove others Elementor styles
+	*
+	* Important: this remove defaults styles from Elementor
+   	* Disabe each and test for dont broke your website functions
+   	*
+	*/
 
 	public function remove_style_elementor () {
 
